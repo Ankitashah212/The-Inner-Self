e0dccf80e289a8ef22271a9a778f2216607d4d61
Kita <ankitashah212@gmail.com>
2017-10-02 10:20:37 -0400

    cleaning up

--- server.js
+++ server.js
@@ -23,36 +23,5 @@ app.listen(PORT, function () {
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes.js')(app);
 
-/*
-var characteristics = [
-    {
-        "name": "Princess",
-        "scores": [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-    },
-    {
-        "name": "Prince",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    }
-];
-
-
-
-
-// Basic route that sends the user first to the AJAX Page
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/home.html"));
-});
-
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
-});
-
-
-// Get all reservations
-app.get("/all", function (req, res) {
-    res.json(characteristics);
-});
-*/
 
 
