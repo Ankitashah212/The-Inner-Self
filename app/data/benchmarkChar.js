91610fb19fed4f107896a310fcd8433748c4aa3e
Kita <ankitashah212@gmail.com>
2017-10-02 15:59:28 -0400

    differences are printing on console

--- app/data/benchmarkChar.js
+++ app/data/benchmarkChar.js
@@ -2,32 +2,32 @@ var characteristics = [
     {
         "name": "Ursula",
         "src" : "https://vignette1.wikia.nocookie.net/disney/images/c/c5/Ursula_transparent.png/revision/latest?cb=20170426170016",
-        "scores": [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+        "scores": [1, 3, 2, 5, 3, 4, 5, 5, 1, 0]
     },
     {
         "name": "Maleficent",
         "src" : "http://www.thecollectionshop.com/Image_Resize_Detail_Image.asp?MiscImage=4024294",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        "scores": [2, 5, 5, 5, 5, 5, 2, 3, 1, 5]
     },
     {
         "name": "Scar",
         "src" : "https://upload.wikimedia.org/wikipedia/en/4/4d/Scar_lion_king.png",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        "scores": [5, 3, 1, 1, 2, 5, 5, 1, 4, 1]
     },
     {
         "name": "Jaffar",
         "src" : "http://1.bp.blogspot.com/_3NbEuEB-o9Y/Sc58oM7sFkI/AAAAAAAACEc/Y8nE4SSikg4/s400/Jaffar.png",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        "scores": [5, 1, 3, 4, 3, 5, 1, 2, 1, 3]
     },
     {
         "name": "Captain Hook",
         "src" : "https://images-na.ssl-images-amazon.com/images/I/41dLWxpwtFL._SX300_.jpg",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        "scores": [5, 1, 4, 1, 1, 5, 2, 3, 4, 2]
     },
     {
         "name": "Evil Queen",
         "src" : "https://vignette.wikia.nocookie.net/disney/images/f/f2/SW_Queen.jpg/revision/latest/scale-to-width-down/250?cb=20130630090059",
-        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        "scores": [3, 1, 5, 5, 5, 5, 5, 3, 1, 5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,31 +5,58 @@ var path = require('path');
 
 // ROUTING
 
-module.exports = function(app){
+module.exports = function (app) {
 
-// API GET Requests 
-    
-    app.get('/api/char', function(req, res){
+    // API GET Requests 
+
+    app.get('/api/char', function (req, res) {
 
         res.json(chars);
 
-        console.log(chars);
+        //console.log(chars);
 
     });
 
-// API Requests
-    
-    app.post('/api/char', function(req, res){
+    // API Requests
+
+    app.post('/api/char', function (req, res) {
 
         var newChar = req.body;
-      
-        console.log(newChar.name);
-        console.log(newChar);
-        console.log(newChar.qualities);
+       
+        console.log(newChar.qualities.length);
+
+        //fun name
+        var myDifferences = [];
+
+        //initializing the array with 0 differences 
+        for (let q = 0; q < chars.length; q++) {
+            myDifferences[q] = 0;
+        }
+
+        console.log("before");
+        console.log(myDifferences);
+    
+
+        //for each input benchmark (question)
+        for (let i = 0; i < newChar.qualities.length; i++) {
+            //loop thru each villains answer for that particular field
+            for (let j = 0; j < chars.length; j++) {
+                //push differences in array mydifferences
+                //the order of difference of a character will be same as they are defined in chars array.
+                var diff = Math.abs(newChar.qualities[i] - chars[j].scores[i])
+                diff = diff + myDifferences[j];
+                myDifferences.splice(j, 1);
+                myDifferences.splice(j, 0, diff);
+             
+            }
+
+        }
+        console.log("after");
+        console.log(myDifferences);
         
         chars.push(req.body);
         res.json(chars[0]);
-        
+
     });
 
 };
