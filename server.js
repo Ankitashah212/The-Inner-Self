16865a40844fdda84d22ed8dc85fbb65b22fb776
Kita <ankitashah212@gmail.com>
2017-10-01 20:05:05 -0400

    all routes working

--- server.js
+++ server.js
@@ -11,6 +11,77 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
+var characteristics = [
+    {
+        "name": "Princess",
+        "scores": [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+    },
+    {
+        "name": "Prince",
+        "scores": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+];
+
+
+
+
+// Basic route that sends the user first to the AJAX Page
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+
+// Get all reservations
+app.get("/all", function (req, res) {
+    res.json(characteristics);
+});
+
+/*
+// Search for Specific Reserver (or all reservations) - provides JSON
+
+app.get("/api/:reservations?", function (req, res) {
+    var chosen = req.params.reservations;
+
+    if (chosen) {
+        console.log(chosen);
+
+        for (var i = 0; i < reservations.length; i++) {
+            if (chosen === reservations[i].uniqueId) {
+                return res.json(reservations[i]);
+            }
+        }
+        return res.json(false);
+    }
+    return res.json(reservations);
+});
+
+
+
+// Create New Reservations - takes in JSON input
+
+app.post("/api/new", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newreservation = req.body;
+    newreservation.uniqueId = newreservation.name.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newreservation);
+
+    reservations.push(newreservation);
+
+    res.json(newreservation);
+});
+*/
